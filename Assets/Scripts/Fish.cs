fd6084249147a975edc6da3d506bfe45fbdcc6e5
Glenn Smith <grsmith97@gmail.com>
2023-09-28 13:42:15 -0700

    Initial commit

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+  [SerializeField]
+  private float jumpForce = 300f;
+  private Rigidbody2D rb;
+
+  private void Awake()
+  {
+    rb = GetComponent<Rigidbody2D>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetButtonDown("Fire1"))
+    {
+      rb.velocity = Vector2.zero;
+      GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce);
+    }
+  }
+}
---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1f;
+    [SerializeField]
+    private float randomOffset = 2f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Time.deltaTime * Vector3.left * speed;
+        if (transform.position.x <= -15) {
+          float randomHeight = UnityEngine.Random.Range(-randomOffset, randomOffset);
+          transform.position = new Vector3(10, randomHeight + 1f, transform.position.z);
+        }
+    }
+}
